9032e0a55acbad06a5bb8fa83edee1580a3280ff
sony <user@FDLMC239-MacBook-Pro.local>
2014-07-29 18:27:16 +0530

    Static file serve erorr fix

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var requestHandlers = require('./library/requestHandlers');
 //url mapping
 var handle = {}
 handle["/"] = requestHandlers.handleStaticFiles;
-//handle["/static"] = requestHandlers.handleStaticFiles;
+handle["static"] = requestHandlers.handleStaticFiles;
 
 server.start(router.route, handle);
